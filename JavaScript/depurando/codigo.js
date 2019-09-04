e7b7dcd4b460055dcb97f129df411e6ad158ca3c
simone <simone.berbert@gmail.com>
2019-09-04 15:50:36 -0300

    Organizando pastas

--- JavaScript/depurando/codigo.js
+++
@@ -1,9 +0,0 @@
-var form = document.forms[0];
-
-form.addEventListener('submit', function novoUsuario(event){
-	event.preventDefault();
-	var nome = document.getElementById('txNome').value;
-	var email = document.getElementById('txtEmail').value;
-
-	alert(nome+' '+email);
-});
